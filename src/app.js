14929e5e94a8bd2105d94372f138cf317de3c986
Naman Joshi <namanjoshi96@gmial.com>
2025-04-28 14:41:06 +0530

    exploring get,post,delete

--- src/app.js
+++ src/app.js
@@ -2,17 +2,28 @@ const express = require('express');
 
 const app = express();
 
-app.get("/",(req,res)=>{
-    res.send("Hello from server")
+app.use("/user",(req,res)=>{
+    res.send("HAHAHAHA")
 })
 
-app.get("/hello",(req,res)=>{
-    res.send("Hello from server hello")
+
+app.get("/user",(req,res)=>{
+    res.send({firstname:"Naman",lastname:"Joshi"})
 })
 
-app.get("/test",(req,res)=>{
+app.post("/user",(req,res)=>{
+    res.send("Data successfullysaved to database")
+})
+app.delete("/user",(req,res)=>{
+    res.send("Deleted Successfully")
+})
+
+
+app.use("/test",(req,res)=>{
     res.send("Hello from /test")
 })
+
+
 
 app.listen(7777,()=>{
     console.log("Server is succcessfully created")
