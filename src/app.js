6f04b7c145392eac3222c7687e4dd6dcb66d8d44
Naman Joshi <namanjoshi96@gmial.com>
2025-04-29 01:01:41 +0530

    get APIs

--- src/app.js
+++ src/app.js
@@ -6,33 +6,50 @@ const User = require("./model/user")
 
 const app = express();
 
-app.post("/signup",async(req,res)=>{
+app.use(express.json())
 
-    
 
-    const data = new User( {
-        firstName:"Rohit",
-        lastName:"Sharma",
-        email:"Rohi@sharma.com",
-        password:"rohit30",
-       
-    })
+app.get("/feed",async(req,res)=>{
 
     try{
-
-        await data.save();
-
-    res.send("User addes Successfully")
-
+        const data = await User.find({});
+        res.send(data);
     }
     catch(err){
+        res.status(404).send("Something went wrong")
+    }
+})
 
+
+app.get("/user",async(req,res) => {
+ 
+    const userEmail = req.body.email;
+
+    try{
+        const data = await User.findOne({email:userEmail})
+        res.send(data)
+    }
+    catch(err){
+        res.status(404).send("Something went wrong")
+    }
+
+})
+
+
+
+app.post("/signup",async(req,res)=>{
+
+    const data = new User(req.body)
+
+    try{
+        await data.save();
+        res.send("User addes Successfully")
+    }
+    catch(err){
         res.status(400).send("Error saving the user:"+ err.message)
     }
 
-    
-
-})
+    })
 
 database()
 .then(()=>{
